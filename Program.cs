fbda271d6c25e1a834dbc4b4d3eafd79bca2e690
Noves67 <nouf23gh@gmail.com>
2024-11-25 11:02:06 +0300

    class1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using system;
+publce class Program
+{
+ publce stice void Mian(string[]arges) 
+ string =Name;
+ Name =" NOUF";
+int age = 24;
+string Grade = good
+consol.writline(" Name: [Name], age:[Age], Grade:[Grade]");
+}
+
